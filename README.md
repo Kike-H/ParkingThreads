f249967edef0297abfe76484c70937dfd07cf69f
Manuel E. Hernandez <kicke10h@gmail.com>
2021-10-10 14:42:13 -0500

    Sincronization

--- README.md
+++ README.md
@@ -4,3 +4,31 @@ Make a program that calculate the earnings of a parking
 In this parking can stay cars and motorcycles the price is the same for all, by the way the price is of $ 20, no matter how long time stay in it. `Use threads` 
 
 ## How to run?
+
+### Linux/Mac OS 
+
+> `Compile` 
+
+```bash
+ ./.compile/compiler.sh
+```
+
+> Execute 
+
+```bash
+./.compile/execute.sh
+```
+
+### Windows 
+
+> Compile 
+
+```cmd
+.\.compile\compiler.bat
+```
+
+> Execute 
+
+```cmd
+.\.compile\execute.bat
+```
--- src/com/app/App.java
+++ src/com/app/App.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 class App {
 	public static void main(String[] args) throws InterruptedException {
+		// Variables
 		Scanner scan = new Scanner(System.in);
 		Count count = new Count();
 
+		// Get the cars 
 		System.out.print("Introduce the amount of cars: ");
 		int amount_cars = scan.nextInt();
 		Thread[] cars = new Thread[amount_cars];
 		
+		// Get the motorcycles
 		System.out.print("Introduce the amount of motorcycles: ");
 		int amount_motorcycles = scan.nextInt();
 		Thread[] motorcycles = new Thread[amount_motorcycles];
@@ -19,28 +22,31 @@ class App {
 
 		scan.close();
 
+		// Make the threads for each vehicle
 		for (int i = 0; i < amount_cars; i++) {
 			ThreadVehicle tc = new ThreadVehicle(count);
 			tc.setName("Car "+(i+1));
 			cars[i] = tc;
+			cars[i].start(); // Execute the thread for car
 		}
 		
 		for (int i = 0; i < amount_motorcycles; i++) {
 			ThreadVehicle tm = new ThreadVehicle(count);
 			tm.setName("Motorcycle "+(i+1));
 			motorcycles[i] = tm;
+			motorcycles[i].start(); // Execute the thread for motorcycle
 		}
 
 		for (Thread car : cars) {
-			car.start();
+			car.join();
 			Thread.sleep(200);
 		}
 
 		for (Thread motorcycle : motorcycles) {
-			motorcycle.start();
+			motorcycle.join();
 			Thread.sleep(200);
 		}
 
-		System.out.println("The total amount is of: "+count.getCount());
+		System.out.println("The total amount for "+( amount_motorcycles + amount_cars )+" vehicles is : "+count.getCount());
 	} 
 }
